e2088d2bc55c96983d426b2cb8f19eb6240f1aa9
sapralakshya <88082433+sapralakshya@users.noreply.github.com>
2022-11-07 12:12:34 +0530

    Add files via upload

---
+++ list_6.py
@@ -0,0 +1,11 @@
+list1=[12,3,9,4,1,2]
+triplet_sum=24
+list2=[]
+for i in range(0,len(list1)):
+    for j in range (i+1,len(list1)):
+        for k in range (j+1,len(list1)):
+            if (list1[i]+list1[j]+list1[k]==triplet_sum):
+                list2.append(list1[i])
+                list2.append(list1[j])
+                list2.append(list1[k])
+print(list2)
