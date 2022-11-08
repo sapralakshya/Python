278946edd51eb1ccca802919a9b7d15b4861a68c
sapralakshya <88082433+sapralakshya@users.noreply.github.com>
2022-11-08 13:32:19 +0530

    Update Sum_of_all_elements.py

--- Sum_of_all_elements.py
+++ Sum_of_all_elements.py
@@ -2,10 +2,12 @@
 arr=[]
 count=0
 n=int(input("enter the no of elements:" ))
-for i in range(0,n):
+for k in range(0,n):
     ele=int(input())
     arr.append(ele)
 print(arr)
-for i in arr:
-    count+=i
+i=int(input())
+j=int(input())
+for k in range(i,j+1):
+    count+=arr[k]
 print(count)
